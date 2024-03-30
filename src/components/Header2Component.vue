c953d6e614cdaca9b1804bd01e396c02cae14eb0
iTzMvz <matheus.vidal.zenere@gmail.com>
2024-03-30 15:56:01 -0300

    Corrigindo erros de css

--- src/components/Header2Component.vue
+++ src/components/Header2Component.vue
@@ -4,7 +4,7 @@ import { ref, watch } from 'vue'
 import Recent from 'vue-material-design-icons/History.vue'
 import Account from 'vue-material-design-icons/Account.vue'
 import Menu from 'vue-material-design-icons/Menu.vue'
-import Search from 'vue-material-design-icons/ShoppingSearch.vue'
+import Magnify from 'vue-material-design-icons/Magnify.vue'
 import { searchAnimeList } from '@/api/index'
 
 const menuAberto = ref(false)
@@ -52,25 +52,29 @@ function clear() {
     </nav>
     <div>
       <div :class="pesquisaAberta ? 'search' : 'form'">
-        <div>
-          <input v-model="search" placeholder="Type your text" required="" type="text" />
-          <span class="input-border"></span>
-        </div>
+        <input
+          class="input-search"
+          v-model="search"
+          placeholder="Type your text"
+          required=""
+          type="text"
+        />
+        <span class="input-border"></span>
       </div>
       <div class="results" v-if="animeList[0]">
-        <ul>
-          <button @click="clear()">X</button>
+        <ul class="list-search">
+          <button class="close-list" @click="clear()">X</button>
           <li v-for="anime in animeList" :key="anime.id" class="anime">
-            <RouterLink :to="`/filme/${anime.id}`" >
-              <h3>{{ anime.title }}</h3>
-              <img :src="anime.image" alt="" class="baner" />
+            <RouterLink :to="`/filme/${anime.id}`">
+              <h3 class="anime-name">{{ anime.title.substring(0, 5) }}...</h3>
+              <img class="baner" :src="anime.image" alt="" />
             </RouterLink>
           </li>
         </ul>
       </div>
     </div>
     <div class="header--icons">
-      <Search class="menu-hamburger" @click="pesquisaAberta = !pesquisaAberta" />
+      <Magnify class="menu-hamburger" @click="pesquisaAberta = !pesquisaAberta" />
       <RouterLink to="/user"><Account /></RouterLink>
       <RouterLink to="/recente"><Recent /></RouterLink>
       <Menu class="menu-hamburger" @click="menuAberto = !menuAberto" />
@@ -139,24 +143,29 @@ nav li {
   background-color: transparent;
   width: 100%;
   box-sizing: border-box;
-  padding-inline: 0.5em;
-  padding-block: 0.7em;
   border: none;
   border-bottom: var(--border-height) solid var(--border-before-color);
+}
+.input-search{
+  width: 70%;
+  display: flex;
+  border-radius: 15px;
+  padding: 5px 10px ;
 }
 /* styling of animated border */
 .input-border {
   position: absolute;
   background: var(--border-after-color);
-  width: 0%;
+  width: 0;
   height: 2px;
   bottom: 0;
-  left: 0;
+  margin-left: 4%;
   transition: 0.3s ease-in-out;
 }
 /* Hover on Input */
 input:hover {
   background: var(--input-hovered-color);
+  color: #fff;
 }
 
 input:focus {
@@ -164,23 +173,38 @@ input:focus {
 }
 /* here is code of animated border */
 input:focus ~ .input-border {
-  width: 100%;
+  width: 73%;
+  margin-left: 4%;
 }
 /*Fim do codigo*/
 .results {
   position: absolute;
-  background-color: #ffffff;
+  backdrop-filter: blur(15px);
   color: #000000;
-  padding: 1rem;
-  top: 40px;
+  padding: 2rem 1rem 1rem 1rem;
+  margin: 10px 0;
+  border-radius: 5px;
+  border: 3px solid black;
   z-index: 1;
-  max-width: 10%;
-  max-height: 60%;
+  max-height: 80vh;
+  max-width: 15vw;
   overflow: scroll;
   font-size: 20px;
   flex-wrap: wrap;
   justify-content: center;
   text-align: center;
+
+}
+.close-list{
+  background-color: rgba(0, 0, 0, 0);
+  position: absolute;
+  right: 0;
+  top: 0;
+  margin: 10px 15px 0 0;
+  color: #ffffff;
+}
+.anime-name{
+  color: #ffffff;
 }
 a {
   transition: 0.3s ease-in-out;
@@ -209,7 +233,7 @@ a:hover {
     right: 0;
     text-align: right;
     padding: 10px 16px;
-    z-index: 999999;
+    z-index: 2;
     color: white;
   }
   nav .menu li {
@@ -224,35 +248,31 @@ a:hover {
     box-shadow:
       0 10px 20px rgba(255, 255, 255, 0.19),
       0 6px 6px rgba(75, 69, 69, 0.23);
-    border-radius: 10px;
+    border-radius: 25px;
     right: 0;
+    top: 50px;
     text-align: right;
-    padding: 10px 16px;
-    z-index: 999999;
-    color: white;
+    padding: 5px;
+    z-index: 1;
+  }
+  .input-search{
+    width: auto;
+  }
+  .input-search:hover{
+    background-color: #1f1e1e;
   }
   .results {
-    display: flex;
-    flex-direction: column;
-    position: absolute;
-    background-color: rgba(66, 66, 66, 0.9);
-    box-shadow:
-      0 10px 20px rgba(255, 255, 255, 0.19),
-      0 6px 6px rgba(75, 69, 69, 0.23);
-    border-radius: 10px;
-    right: 0;
-    top: 65px;
-    list-style: none;
+    right: 15px;
+    top: 80px;
     font-size: 30px;
+    min-width: var(--width-of-input);
     max-width: var(--width-of-input);
-    min-width: var(--width-of-input);;
-    max-height: 300px;
-    z-index: 999998;
-    color: white;
+    max-height: 50vh;
     overflow: scroll;
+    z-index: 2;
   }
-  .anime{
-    padding-top: 10px
+  .anime {
+    padding-top: 10px;
   }
   .form {
     display: none;
