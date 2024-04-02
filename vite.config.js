e89ab450c6fffd610b53fd75641fc55bfee5c70b
jonatasperaza <jonatassilvaperaza@gmail.com>
2024-04-01 21:07:53 -0300

    Atualizações do serviço de trabalhadores e ícone de favoritos***
    ***Atualizações do serviço de trabalhadores e ícone de favoritos

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import VueDevTools from 'vite-plugin-vue-devtools'
-import { VitePWA } from 'vite-plugin-pwa';
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,36 +14,30 @@ export default defineConfig({
       registerType: 'autoUpdate',
       includeAssets: ['favicon.svg', 'apple-touch-icon.png', 'masked-icon.svg'],
       manifest: {
-        name: 'Fake Store',
+        name: 'PerazaFlix',
         short_name: 'PerazaFlix',
         description: 'PerazaFlix',
         theme_color: '#ffffff',
         icons: [
           {
-            src: 'pwa-192x192.png',
-            sizes: '192x192',
+            src: 'favicon.png',
+            sizes: '200x200',
             type: 'image/png',
-            purpose: 'any',
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'maskable',
-          },
+            purpose: 'any'
+          }
         ],
         id: 'com.fake-store.app',
         orientation: 'any',
         background_color: '#ffffff',
         start_url: '.',
         launch_handler: {
-          client_mode: ['navigate-existing', 'auto'],
-        },
+          client_mode: ['navigate-existing', 'auto']
+        }
       },
       devOptions: {
-        enabled: true,
-      },
-    }),
+        enabled: true
+      }
+    })
   ],
   resolve: {
     alias: {
